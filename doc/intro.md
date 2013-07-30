6ab7e1723add857fadc18e4142b49bb88d2473a3
Jim Crossley <jim@crossleys.org>
2013-07-29 22:52:58 -0400

    The result of 'lein new cluster-tutorial'

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cluster-tutorial
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/cluster_tutorial/core_test.clj
@@ -0,0 +1,7 @@
+(ns cluster-tutorial.core-test
+  (:use clojure.test
+        cluster-tutorial.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
